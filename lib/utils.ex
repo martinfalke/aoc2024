98d7a5a97ead7af58e82386b23a535c7481629a6
martinfalke <mafalke@kth.se>
2024-12-26 23:45:14 +0100

    add 2d map helper functions in utils

--- lib/utils.ex
+++ lib/utils.ex
@@ -27,6 +27,23 @@ defmodule Utils do
   def remove_trailing_newline([]), do: []
   def remove_trailing_newline([head | [""]]), do: [head]
 
+  @spec get_2d_map_size(list(list())) :: {integer(), integer()}
+  def get_2d_map_size(map) do
+    num_rows = length(map)
+    num_cols = List.first(map) |> length()
+    {num_rows, num_cols}
+  end
+
+  @spec create_2d_map_coordinates(list(list())) :: list()
+  def create_2d_map_coordinates(map) do
+    {num_rows, num_cols} = get_2d_map_size(map)
+    map_coords =
+      0..(num_rows - 1)
+      |> Enum.map(fn r -> 0..(num_cols - 1) |> Enum.map(fn c -> {r, c} end) end)
+      |> List.flatten()
+    map_coords
+  end
+
   def init_day(day, nil) do
     fc = read_input_file(day)
     fc
